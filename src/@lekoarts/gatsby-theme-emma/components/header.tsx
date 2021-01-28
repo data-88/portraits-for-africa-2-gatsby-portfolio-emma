be2b4ee8094c29a45afc9bbc77a9b58bb51d819f
data-88 <andrew@thecyberium.com>
2021-01-28 16:23:39 +0200

    gtag

--- src/@lekoarts/gatsby-theme-emma/components/header.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/header.tsx
@@ -74,6 +74,15 @@ const Header = ({ meta, nav }: HeaderProps) => {
           {isDark ? `Light` : `Dark`}
         </button>
       </div>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-H91ZMBKVMZ"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H91ZMBKVMZ');
+      </script>
     </Flex>
   )
 }
